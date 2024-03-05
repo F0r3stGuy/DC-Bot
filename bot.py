a85123ae85a018f79283d54f21b91c6969d059e7
F0r3stGuy <j.s.bosman@student.utwente.nl>
2024-03-05 02:45:09 +0100

    v2 with slash commands and embed messages

--- bot.py
+++ bot.py
@@ -2,26 +2,17 @@ import os
 import json
 from requests import get
 from urllib import parse
+from colorama import Fore, Style
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
 # -------------------- Setup --------------------
 
-load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-RG_KEY = os.getenv('RG_KEY')
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-
-client = commands.Bot(command_prefix='!', intents=intents)
-
-accountURL = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key="+RG_KEY
-summonerURL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key="+RG_KEY
-entryURL = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{id}?api_key="+RG_KEY
+INFO = f"{Style.BRIGHT}{Fore.LIGHTBLUE_EX}INFO{Fore.RESET}{Style.NORMAL}\t"
+WARN = f"{Style.BRIGHT}{Fore.LIGHTYELLOW_EX}WARN{Fore.RESET}{Style.NORMAL}\t"
 
 SOLO_TIER_TO_ROLE = {
     "CHALLENGER": -2,
@@ -44,78 +35,23 @@ FLEX_TIER_TO_ROLE = {
 UNRANKED_ROLE = -16
 NO_LEAGUE_ROLE = -17
 
-# -------------------- Commands --------------------
+load_dotenv()
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+RG_KEY = os.getenv('RG_KEY')
 
-@client.command(help="To link your league account, so the bot can figure out your rank.",brief="To link your league account")
-async def link(ctx: commands.Context, *param):
-    # Error handling
-    summoner = ' '.join(param)
-    split = summoner.split('#')
-    if len(split) != 2:
-        await ctx.send(f'Please set the parameter of this command to the form: `name#tag`.')
-        return
-    response = get(accountURL.format(name=parse.quote(split[0].strip()), tag=split[1].strip()))
-    if response.status_code != 200:
-        await ctx.send(f"This summoner does not exist... Please check your `name` and `tag` again.")
-        return
-    
-    # Get the already linked members and save the data to links.json
-    with open("links.json", "r") as file:
-        links = json.load(file)
-    member = ctx.author
+accountURL = "https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key="+RG_KEY
+summonerURL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key="+RG_KEY
+entryURL = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{id}?api_key="+RG_KEY
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+# -------------------- Helpers --------------------
+
+async def handleApiDataAndMakeEmbed(links, member: discord.Member, title: str) -> discord.Embed:
     key = str(member.id)
-    summonerData = get(summonerURL.format(puuid=response.json()['puuid'])).json()
-    entryData = get(entryURL.format(id=summonerData['id'])).json()
-    links[key] = {}
-    links[key]['id'] = summonerData['id']
-    links[key]['name'] = summonerData['name']
-    
-    # Prepare message
-    soloData, flexData = None, None
-    for queueType in entryData:
-        if "SOLO" in queueType['queueType']:
-            soloData = queueType
-        if "FLEX" in queueType['queueType']:
-            flexData = queueType
-    await member.remove_roles(*member.roles[1:])
-    if len(entryData) == 0:
-        await member.add_roles(member.guild.roles[UNRANKED_ROLE])
-    message = ""
-    if soloData != None:
-        message += f"You have the current solo queue rank: `{soloData['tier'].title()} {soloData['rank']} {soloData['leaguePoints']} lp`"
-        links[key]['solo'] = soloData['tier']+" "+soloData['rank']
-        await member.add_roles(member.guild.roles[SOLO_TIER_TO_ROLE[soloData['tier']]])
-    else:
-        message += "You are unranked in solo queue..."
-        links[key]['solo'] = "UNRANKED"
-    if flexData != None:
-        message += f"\nYou have the current flex queue rank: `{flexData['tier'].title()} {flexData['rank']} {flexData['leaguePoints']} lp`"
-        links[key]['flex'] = flexData['tier']+" "+flexData['rank']
-        if flexData['tier'] in FLEX_TIER_TO_ROLE:
-            await member.add_roles(member.guild.roles[FLEX_TIER_TO_ROLE[flexData['tier']]])
-        elif soloData == None:
-            await member.add_roles(member.guild.roles[UNRANKED_ROLE])
-    else:
-        message += "\nYou are unranked in flex queue..."
-        links[key]['flex'] = "UNRANKED"
-
-    with open("links.json", "w") as file:
-        json.dump(links, file)
-    await ctx.send(f'You have linked to your league account: `{summoner}`\n'+message)
-
-@client.command(help="To set your roles in the server and show your rank in chat.",brief="To set your roles")
-async def getrank(ctx: commands.Context):
-    # Get the already linked members
-    with open("links.json", "r") as file:
-        links = json.load(file)
-    member = ctx.author
-    if str(member.id) not in links:
-        await ctx.send(f'You are not linked to your league account yet. You can link using `!link name#tag`.')
-        return
     respone = get(entryURL.format(id=links[str(member.id)]['id']))
     if respone.status_code != 200:
-        await ctx.send(f'Something is wrong with the currently linked league account. Try linking again using `!link name#tag`.')
-        return
+        return discord.Embed(description="Something is wrong with the currently linked league account. Try linking again using `link` command.", colour=discord.Colour.red())
     entryData = respone.json()
     soloData, flexData = None, None
     for queueType in entryData:
@@ -123,43 +59,106 @@ async def getrank(ctx: commands.Context):
             soloData = queueType
         if "FLEX" in queueType['queueType']:
             flexData = queueType
+    
+    # Give the right roles and prepare message
+    embed = discord.Embed(title=title, colour=discord.Colour.blurple())
+    embed.set_author(name=links[key]['name'])
 
-    # Give the right roles
     await member.remove_roles(*member.roles[1:])
     if len(entryData) == 0:
         await member.add_roles(member.guild.roles[UNRANKED_ROLE])
-    message = ""
     if soloData != None:
-        message += f"You have the current solo queue rank: `{soloData['tier'].title()} {soloData['rank']} {soloData['leaguePoints']} lp`"
+        soloField = f"{soloData['tier'].title()} {soloData['rank']} {soloData['leaguePoints']} lp"
+        links[key]['solo'] = soloData['tier']+" "+soloData['rank']
         await member.add_roles(member.guild.roles[SOLO_TIER_TO_ROLE[soloData['tier']]])
     else:
-        message += "You are unranked in solo queue..."
+        soloField = "Unranked"
+        links[key]['solo'] = "UNRANKED"
     if flexData != None:
-        message += f"\nYou have the current flex queue rank: `{flexData['tier'].title()} {flexData['rank']} {flexData['leaguePoints']} lp`"
+        flexField = f"{flexData['tier'].title()} {flexData['rank']} {flexData['leaguePoints']} lp"
+        links[key]['flex'] = flexData['tier']+" "+flexData['rank']
         if flexData['tier'] in FLEX_TIER_TO_ROLE:
             await member.add_roles(member.guild.roles[FLEX_TIER_TO_ROLE[flexData['tier']]])
         elif soloData == None:
             await member.add_roles(member.guild.roles[UNRANKED_ROLE])
     else:
-        message += "\nYou are unranked in flex queue..."
-    await ctx.send(message)
+        flexField = "Unranked"
+        links[key]['flex'] = "UNRANKED"
+    
+    with open("links.json", "w") as file:
+        json.dump(links, file)
+    
+    embed.add_field(name=f"Solo/Duo Queue", value=soloField, inline=False)
+    embed.add_field(name=f"Flex Queue", value=flexField, inline=False)
+    return embed
+
+# -------------------- Commands --------------------
+
+@bot.tree.command(name="link", description="Links your league account to discord, so the bot can figure out your rank.")
+@app_commands.describe(summoner="Your summoner name")
+@app_commands.describe(tag="Your tag")
+async def link(interaction: discord.Interaction, summoner: str, tag: str):
+    # Error handling
+    response = get(accountURL.format(name=parse.quote(summoner.strip()), tag=tag.strip()))
+    if response.status_code != 200:
+        embed = discord.Embed(description="This summoner does not exist... Please check your `name` and `tag` again.", colour=discord.Colour.red())
+        await interaction.response.send_message(embed=embed)
+        return
+    
+    # Get the already linked members
+    with open("links.json", "r") as file:
+        links = json.load(file)
+    member = interaction.user
+    key = str(member.id)
+    response = get(summonerURL.format(puuid=response.json()['puuid']))
+    if response.status_code != 200:
+        embed = discord.Embed(description="This summoner does not exist... Please check your `name` and `tag` again.", colour=discord.Colour.red())
+        await interaction.response.send_message(embed=embed)
+        return
+    summonerData = response.json()
+    links[key] = {}
+    links[key]['id'] = summonerData['id']
+    links[key]['name'] = summoner+'#'+tag
+    
+    embed = await handleApiDataAndMakeEmbed(links, member, "Linked!")
+    await interaction.response.send_message(embed=embed)
+
+@bot.tree.command(name="roles", description="Updates your roles in the server according to your rank.")
+async def roles(interaction: discord.Interaction):
+    # Get the already linked members
+    with open("links.json", "r") as file:
+        links = json.load(file)
+    member = interaction.user
+    if str(member.id) not in links:
+        embed = discord.Embed(description="You are not linked to your league account yet. See the `link` command.", colour=discord.Colour.red())
+        await interaction.response.send_message(embed=embed)
+        return
+    
+    embed = await handleApiDataAndMakeEmbed(links, member, "Ranks")
+    await interaction.response.send_message(embed=embed)
         
 
 # -------------------- Events --------------------
     
 # TODO: Add the on join event for the No League? role.
 
-@client.event
+@bot.event
 async def on_message(message: discord.Message):
     if message.author.bot:
         return
     else:
-        await client.process_commands(message)
+        await bot.process_commands(message)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
-    guild = client.guilds[0]
+    guild = bot.guilds[0]
+    
+    # Sync slash commands
+    try:
+        synced = await bot.tree.sync()
+        print(f"{INFO}Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
     # Get the already linked members
     with open("links.json", "r") as file:
@@ -171,11 +170,13 @@ async def on_ready():
         if str(member.id) not in links:
             # await member.remove_roles(*member.roles[1:])
             # await member.add_roles(guild.roles[NO_LEAGUE_ROLE])
-            print(f"- {member.name}, id: {member.id} is not in the list")
+            print(f"{WARN}{member.name}, id: {member.id} is not in the list")
 
     with open("links.json", "w") as file:
         json.dump(links, file)
 
+    print(f"{INFO}{bot.user} is ready to go!")
+
 # -------------------- Run --------------------
 
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
